8b7b4e88da1f5f44d0d672992ad4c478d760458e
Gering <GDong01@wildcats.jwu.edu>
2020-05-02 22:55:56 -0400

    created car interface

---
+++ src/app/cars/car-list.compenent.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'
+
+@Component({
+
+
+})
+
+export class carListComponent{
+    pageTitle : string = "Car List"
+
+}
+
---
+++ src/app/cars/cars.ts
@@ -0,0 +1,13 @@
+export interface ICar{
+    location: string;
+    mileage: number;
+    transmition: string;
+    exterior_color: string;
+    interior_color: string;
+    maximum_seating: string;
+    gas_mileage: string;
+    engine: string;
+    driverTrain: string;
+    fuelType: string;
+
+}
