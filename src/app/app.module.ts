a99c718c8dcc3bb2ac5ed3c6f961aba65c358805
Gering <GDong01@wildcats.jwu.edu>
2020-05-03 01:18:40 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { register } from './registeration/register.component';
 import {FormsModule} from '@angular/forms'
+import {account} from './account/account.component'
+import {login} from './login/login.component'
 
 @NgModule({
   declarations: [
-    AppComponent, register
+    AppComponent,register,login, account 
   ],
   imports: [
     BrowserModule, FormsModule
